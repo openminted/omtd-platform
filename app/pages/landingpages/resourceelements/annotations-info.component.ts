fcd36056110b35f390bfcdd263930315282d937e
smartziou <stefania.martziou@imis.athena-innovation.gr>
2017-12-16 22:03:13 +0200

    Finished the annotations display in the corpus landing page

--- app/pages/landingpages/resourceelements/annotations-info.component.ts
+++ app/pages/landingpages/resourceelements/annotations-info.component.ts
@@ -7,17 +7,12 @@ import { AnnotationInfo } from "../../../domain/openminted-model";
 @Component({
     selector: 'annotations-info',
     templateUrl: './annotations-info.component.html',
+    styleUrls:  ['../landing-page.component.css'],
 })
 
 export class AnnotationsInfoComponent {
 
     @Input() annotations: AnnotationInfo[];
 
-    slides = [
-        {img: "http://placehold.it/350x150/000000"},
-        {img: "http://placehold.it/350x150/111111"},
-        {img: "http://placehold.it/350x150/333333"},
-        {img: "http://placehold.it/350x150/666666"}
-    ];
-    slideConfig = {"slidesToShow": 2, "slidesToScroll": 2};
+    slideConfig = {"slidesToShow": 3, "slidesToScroll": 3};
 }
